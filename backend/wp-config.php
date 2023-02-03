6a4bdf21d4d238a40c5ace95fc62d809df8aa527
Daniele Callegaro <daniele.callegaro.90@gmail.com>
2023-02-03 18:57:21 +0100

    Refactor folder, added backend

---
+++ backend/wp-config.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'caiman' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'password' );
+
+/** Database hostname */
+define( 'DB_HOST', 'db' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         ',g/m`)dBGT{>RoX;=f?J Uw_zL}9Eyuqx9I$[`fw1Z3$Unu9NTnw,)2LozYJ<s[[' );
+define( 'SECURE_AUTH_KEY',  '9MD)q}L}N64B:9JWq]E2hs+ic5}ocQ;<WBa.-}C6>Z#G2tpN2 M9?f!RL8i|%8h!' );
+define( 'LOGGED_IN_KEY',    'sjqKWvd|hg a>x*l`HI.ROM&t *-evV,A(R-upw 97e/afd<.tSUrZ}2W<5^vk!>' );
+define( 'NONCE_KEY',        'J??[a6^9Y3#c.|9mB,Y7daD:,X}MN1}ZdEzaQadO)<K<_zw0j[Hy&JEn[@aT>aLW' );
+define( 'AUTH_SALT',        ']Qso3f}3;]k?[y45ReFarn>THDJmp]f(b?gc9:no=o&gyeoE=r>ygk=~i4uLI+#=' );
+define( 'SECURE_AUTH_SALT', 'f:hpp-.|va*MX[{xh?eX(vle.^ZXzQ]Y%&7!,q{pId?q*c**/Q(S3x@mH>fGB%Ku' );
+define( 'LOGGED_IN_SALT',   ')+CXA>x$45yFgvw-6y#wUavVQ?q{k<L)%G`&Jawz}SC9jy4t]BL3WJx: zIP.)Fk' );
+define( 'NONCE_SALT',       '{x1498:K? 4.v<Zwpg1*.?pG;U(`-fZNs2#0M*N*=,4Kb|J:KG8mo]z$?W$w).Gf' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+define('JWT_AUTH_SECRET_KEY', 'tokenagua');
+define('JWT_AUTH_CORS_ENABLE', true);
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ backend/wp-content/mu-plugins/custom.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Plugin Name: Caiman Web Functions
+ * Plugin URI:  https://www.micronovasrl.com
+ * Description: Caiman Web Functions.
+ * Version:     2.1.3
+ * Author:      Daniele Callegaro
+ * Author URI:  https://www.micronovasrl.com
+ * License:     GPL-3.0
+ * Text Domain: caiman
+ * Domain Path: /languages
+ *
+ * @package caiman
+ */
+
+add_filter(
+    'jwt_auth_payload',
+    function ( $payload, $user ) {
+        $u = array();
+        $u['id'] = $user->ID;
+        $u['name'] = $user->display_name;
+        $u['email'] = $user->user_email;
+        $u['roles'] = $user->roles;
+        $payload["email"] = $user->user_email;
+        $payload["id_app"] = $user->user_email;
+        $payload["id_brand"] = "AguaManager";
+        $payload["customer_code"] = "195764";
+        $payload["data"]["user"] = $u;
+        $payload["auth0"] = "true";
+        $payload["id"] = "464D7C31-0BBD-462D-A7F8-C03913EED030";
+        return $payload;
+    },
+    10,
+    2
+);
+
+add_filter(
+    'jwt_auth_expire',
+    function ( $expire, $issued_at ) {
+        // Modify the "expire" here.
+        return time() + 86400;
+    },
+    10,
+    2
+);
+
+function add_default_options_page() {
+    if( function_exists('acf_add_options_page') ) {
+        acf_add_options_page();
+    }
+}
+add_action( 'plugins_loaded', 'add_default_options_page' );
+
+add_filter( 'upload_mimes', 'set_other_mime_types', 1, 1 );
+
+function set_other_mime_types( $mime_types ) {
+  $mime_types['snet2'] = 'text/plain';
+
+  return $mime_types;
+}
+
+function register_caiman_rest_api()
+{
+    /*remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+    add_filter( 'rest_pre_serve_request', function( $value ) {
+      header( 'Access-Control-Allow-Origin: *' );
+      header( 'Access-Control-Allow-Methods: GET' );
+      header( 'Access-Control-Allow-Credentials: true' );
+      header( 'Access-Control-Expose-Headers: Link', false );
+      return $value;
+    } );*/
+    register_rest_route(
+        'caiman/v1',
+        '/me',
+        array(
+            'methods' => 'GET',
+            'callback' => 'caiman_get_user',
+            'permission_callback' => '__return_true'
+        )
+    );
+    register_rest_route(
+        'caiman/v1',
+        '/options',
+        array(
+            'methods' => 'GET',
+            'callback' => 'caiman_get_options',
+            'permission_callback' => '__return_true'
+        )
+    );
+
+}
+
+
+add_action( 'rest_api_init', 'register_caiman_rest_api' );
+
+function caiman_get_user(WP_REST_Request $request)
+{
+    return new WP_REST_Response(wp_get_current_user(), 200);
+}
+
+function caiman_get_options(WP_REST_Request $request)
+{
+    $ret = new \stdClass();
+    $ret->agua_endpoint = get_field('agua_endpoint', 'option');
+    $ret->agua_hostname = get_field('agua_hostname', 'option');
+    $ret->agua_id_brand = intval(get_field('agua_id_brand', 'option'));
+    $ret->agua_customer_code = intval(get_field('agua_customer_code', 'option'));
+
+    return new WP_REST_Response($ret, 200);
+}

7ec791206f81a86f0afb96588ed8b98f6aff61c7
Daniele Callegaro <daniele.callegaro.90@gmail.com>
2023-02-23 21:04:00 +0100

    Added caiman_get_unsecure_file

--- backend/wp-content/mu-plugins/caiman/lib/api.php
+++ backend/wp-content/mu-plugins/caiman/lib/api.php
@@ -10,7 +10,26 @@ add_action( 'rest_api_init', function(){
             'permission_callback' => '__return_true'
         )
     );
+    register_rest_route(
+        'caiman/v1',
+        '/unsecure_file/(?P<attachment>\d+)',
+        array(
+            'methods' => 'GET',
+            'callback' => 'caiman_get_unsecure_file',
+            'permission_callback' => '__return_true'
+        )
+    );
 });
+
+function caiman_get_unsecure_file( WP_REST_Request $request ) {
+    $params = $request->get_params();
+    $md5 = get_attached_file($params["attachment"]);
+    $url = wp_get_attachment_url($params["attachment"]);
+    $parts = parse_url($url);
+    $path = "/files" . str_replace("/backend/wp-content/uploads", "", $parts["path"]);
+    $unsecure_path = "http://" . $parts["host"] . $path;
+    return new WP_REST_Response(array("md5" => $md5, "path" => $unsecure_path), 200);
+}
 
 function caiman_get_gateway( WP_REST_Request $request ) {
     $params = $request->get_params();

07db5777ecff7066d013969c3a8ae45934818ef1
dacalleg <daniele.callegaro.90@gmail.com>
2024-04-16 21:22:03 +0200

    Added option generator

--- frontend/src/app/modules/config/components/edit-options/edit-options.component.ts
+++ frontend/src/app/modules/config/components/edit-options/edit-options.component.ts
@@ -1,32 +1,45 @@
-import { Component, Input } from '@angular/core';
-import { Variable } from 'src/app/classes/interfaces';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Variable} from 'src/app/classes/interfaces';
 
 @Component({
   selector: 'app-edit-options',
   templateUrl: './edit-options.component.html',
   styleUrls: ['./edit-options.component.scss']
 })
-export class EditOptionsComponent {
+export class EditOptionsComponent implements OnChanges {
 
 
   @Input() variable: Variable | null;
-  newOption: string;
+  list: string;
+
   constructor() {
     this.variable = null;
-    this.newOption = "";
+    this.list = "";
   }
 
-  addOption() {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.variable && this.variable.values)
+      this.list = this.variable.values.map(item => item[0] + ":" + item[1]).join("\n")
+  }
+
+  onListChange($event: string) {
+    this.list = $event;
     if (this.variable) {
-      const pieces = this.newOption.split(":");
-      if (!this.variable.values)
-        this.variable.values = [];
-      this.variable.values.push([pieces[0].trim(), pieces[1].trim()]);
+      const lines = $event.split("\n");
+      this.variable.values = lines.map(line => line.split(":"))
     }
   }
 
-  removeOption(key: string) {
-    if (this.variable)
-      this.variable.values = this.variable?.values?.filter(k => k[0] !== key)
+  generateAllOptions() {
+    if (this.variable && this.variable.values) {
+      const step = this.variable?.step || 1;
+      const min = this.variable?.min || 0;
+      const max = this.variable?.max || 0;
+      for (let i = min; i <= max; i = i + step) {
+        const name = this.variable.formatstring.replace("{0}", "" + i);
+        this.variable?.values?.push(["" + i, name]);
+      }
+      this.list = this.variable.values.map(item => item[0] + ":" + item[1]).join("\n")
+    }
   }
 }

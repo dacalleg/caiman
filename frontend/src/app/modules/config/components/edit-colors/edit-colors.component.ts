e56aee66c4a720675c73714b54607b20f3cf0713
dacalleg <daniele.callegaro.90@gmail.com>
2024-04-17 20:46:38 +0200

    Updates

---
+++ frontend/src/app/modules/config/components/edit-colors/edit-colors.component.ts
@@ -0,0 +1,41 @@
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Variable, VariableColor} from "../../../../classes/interfaces";
+
+@Component({
+  selector: 'app-edit-colors',
+  templateUrl: './edit-colors.component.html',
+  styleUrls: ['./edit-colors.component.scss']
+})
+export class EditColorsComponent implements OnChanges {
+  @Input() variable: Variable | undefined;
+  list: string;
+
+  constructor() {
+    this.list = "";
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.variable && this.variable.colors)
+      this.list = this.variable.colors.map(item => item.condition.operator + ":" + item.condition.value + ":" + item.color ).join("\n")
+  }
+
+  onListChange($event: string) {
+    this.list = $event;
+    if (this.variable) {
+      const lines = $event.split("\n");
+      this.variable.colors = lines.map(line => {
+        const pieces = line.split(":");
+        return {
+          condition: {
+            operator: pieces[0],
+            value: +pieces[1]
+          },
+          color: pieces[2],
+
+        } as VariableColor
+      })
+    }
+  }
+
+
+}

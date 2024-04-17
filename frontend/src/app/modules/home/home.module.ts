968d466917e804bccb8a56fd00b3c996d8bc5082
dacalleg <daniele.callegaro.90@gmail.com>
2024-04-17 20:45:59 +0200

    Added colors

--- frontend/src/app/modules/home/home.module.ts
+++ frontend/src/app/modules/home/home.module.ts
@@ -42,6 +42,7 @@ import { ProfileComponent } from '../profile/components/profile/profile.componen
 import { OperationTitlePipe } from './pipes/operation-title.pipe';
 import { UnescapePipe } from './pipes/unescape.pipe';
 import {SortGroupPipe} from "./pipes/sort-group.pipe";
+import { ColorValuePipe } from './pipes/color-value.pipe';
 
 const icons = {
   Eye,
@@ -91,7 +92,8 @@ const icons = {
     ProfileComponent,
     OperationTitlePipe,
     UnescapePipe,
-    SortGroupPipe
+    SortGroupPipe,
+    ColorValuePipe
   ],
   imports: [
     CommonModule,

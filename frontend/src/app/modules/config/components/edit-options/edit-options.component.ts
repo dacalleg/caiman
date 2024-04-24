5881a97d599e9737f67f79ababc8a640f1dc0dcd
dacalleg <daniele.callegaro.90@gmail.com>
2024-04-24 16:01:03 +0200

    Bugfix

--- frontend/src/app/modules/config/components/edit-options/edit-options.component.ts
+++ frontend/src/app/modules/config/components/edit-options/edit-options.component.ts
@@ -62,8 +62,8 @@ export class EditOptionsComponent implements OnChanges {
         this.list = res;
       else
         this.list += "\n" + res;
+      this.onListChange(this.list);
     }
-    this.onListChange(this.list);
   }
 
   computeGeneratorFunction(value: number)
